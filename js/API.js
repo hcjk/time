5ef54db2de9dae4bbca7160d85b44a14ee82ee6e
Henry Kaufman <hcjk@users.noreply.github.com>
2016-09-17 15:16:44 -0600

    Might have fixed cors :confused:

--- js/API.js
+++ js/API.js
@@ -2,7 +2,7 @@ angular.module('app')
     .factory('API', ['API_KEY', '$http', function (API_KEY, $http) {
         var API = {};
 
-        API.API_BASE = 'https://www.rescuetime.com/anapi/';
+        API.API_BASE = 'https://cors.now.sh/https://www.rescuetime.com/anapi/';
 
         API.getTimeData = function (callback) {
             $http.get(API.API_BASE + 'daily_summary_feed?key=' + API_KEY)
